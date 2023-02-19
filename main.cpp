7580d354c9631f25c2881d9a3851bd4eaadd94c1
Philip Park <42254947+philippark@users.noreply.github.com>
2023-02-18 21:18:00 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,29 +8,58 @@
 #include<random>
 #include <cctype>
 #include <fstream>
-
 #include "message.h"
 
-int main(int argc, char* argv[]){
-    if (std::string(argv[3]) == "--caesar"){
-        Message m;
-        std::map<char, char> key = m.caesar_encrypt(3, argv[1], argv[2]);
-        m.decrypt(key, argv[2]);
+void print_title_screen(){
+    std::ifstream title_screen("title_art.txt");
+    std::string line;
+    while (getline(title_screen, line)){
+        std::cout << line << std::endl;
     }
 
-    if (std::string(argv[3]) == "--monoalphabetic"){
+
+}
+
+void options(){
+
+}
+
+int main(int argc, char* argv[]){
+    print_title_screen();
+    std::string in, out, cipher;
+
+    while(true){
+        std::cout << "cyphen> ";
+        std::string line;
+        getline(std::cin, line);
+        if (line == "q"){
+            break;
+        }
+
+        
+    }
+
+
+
+    if (cipher == "--caesar"){
+        Message m;
+        std::map<char, char> key = m.caesar_encrypt(3, in, out);
+        m.decrypt(key, out);
+    }
+
+    if (cipher == "--monoalphabetic"){
         Message m;
         std::map<char, char> key2 = m.monoalphabetic_encrypt(argv[1], argv[2]);
         m.decrypt(key2, argv[2]);
     }
 
-    if (std::string(argv[3]) == "--homophonic"){
+    if (cipher == "--homophonic"){
         Message m;
         std::map<char, std::vector<std::string>> key = m.homophonic_encrypt(argv[1], argv[2]);
         m.homophonic_decrypt(key, argv[2]);
     }
 
-    if (std::string(argv[3]) == "--vigenere"){
+    if (cipher == "--vigenere"){
         std::cout << "key word: ";
         std::string keyword;
         std::cin >> keyword;
@@ -40,5 +69,6 @@ int main(int argc, char* argv[]){
 
     }
 
+    
 
 }
